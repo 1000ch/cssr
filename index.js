4c89329e884e2ad95fdce35488a2508a79e4d075
Hiroki Tani <service@inkdesign.jp>
2014-08-15 16:03:03 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response){
+	response.render('index', {name: '10000ch'});
+});
+
+app.listen(3000);
