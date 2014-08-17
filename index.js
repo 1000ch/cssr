ed359e585cea7661c6547f88b63fd25183b1a9c1
1000ch <shogo.sensui@gmail.com>
2014-08-18 00:50:36 +0900

    wip

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+var ucss = require('ucss');
 var express = require('express');
-
 var app = express();
 
 app.set('view engine', 'jade');
@@ -7,7 +7,19 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response){
-	response.render('index', {name: '10000ch'});
+  response.render('index', {});
+});
+
+app.get('/api/cssr', function (request, response) {
+  console.log(request.params);
+  console.log(request.query);
+
+  if (request.query.url) {
+    var urls = [request.query.url];
+  }
+  response.json({
+    foo: 'bar'
+  });
 });
 
 app.listen(process.env.PORT || 3000);
