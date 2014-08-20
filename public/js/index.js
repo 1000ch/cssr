b921922cd7ab1b67c655c67e8f99f877756db889
1000ch <shogo.sensui@gmail.com>
2014-08-20 11:58:34 +0900

    add base impl

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,8 @@ $(function () {
 
   var $button = $('button');
   var $input = $('input');
+  var $resultList = $('#js-result-list');
+  var resultItemTemplate = document.querySelector('#js-result-item-template');
 
   $button.on('click', function () {
     console.log('button is clicked');
@@ -13,6 +15,20 @@ $(function () {
         url: $input.val()
       }
     }).done(function (data) {
+      
+      var errors = data.load_errors;
+      var selectors = data.selectors;
+
+      var items = [];
+      var clone = null;
+      Object.keys(selectors).forEach(function (selector) {
+        clone = document.importNode(resultItemTemplate.content, true);
+        clone.querySelector('code').textContent = selector;
+        items.push(clone);
+      });
+
+      $resultList.empty().append(items);
+
       console.log(data);
     }).fail(function (jqXHR, status, error) {
       console.log(error);
