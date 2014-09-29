333b1708d272f647b45def51ae66911502eb57d0
1000ch <shogo.sensui@gmail.com>
2014-09-29 16:28:41 +0900

    add cheet command

--- src/js/app.js
+++ src/js/app.js
@@ -15,7 +15,20 @@ $(function () {
     $zangief.addClass('js-loading loading is-hidden');
     $('css-loading').replaceWith($zangief);
   });
-  
+
+  // Cheet
+  cheet('c s s r', function () {
+    // remove default theme
+    $('link[rel="stylesheet"]').filter(function () {
+      return this.getAttribute('href') === '/css/theme.min.css';
+    }).remove();
+
+    // replace loading icon with him
+    var $zangief = $(document.createElement('x-zangief'));
+    $zangief.addClass('js-loading loading is-hidden');
+    $('css-loading').replaceWith($zangief);
+  });
+
   // Bindings
   var $navigation = $('nav[role=navigation]');
   var $cssList = $('#js-css-list');
